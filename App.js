23c0c284243ddc3954171b894c606bc73d078695
prithul-paul <ptithulpaul@gmail.com>
2025-07-20 03:01:19 +0530

    Install "Parcel",  "React", "React-dom" library and used the replace of CDNs

--- App.js
+++ App.js
@@ -1,23 +1,5 @@
-/**
- * 
- * <div class="parent">
-*      <div class="child1">
-*           <h1>This is heading</h1>
-            <p>This is paragraph</p>
-*      </div>
-        <div class="child2">
-*           <h1>This is heading</h1>
-            <p>This is paragraph</p>
-*      </div>
- * </div>
- * 
- * 
- * 
- * 
- * 
- */
-
-
+import React from "react";
+import ReactDOM from "react-dom/client";
 // const heading = React.createElement("h1", 
 // {
 //     id: "heading", 
@@ -25,25 +7,16 @@
 //     "data-heading": "h1"
 // }, "Hello world from React!!");
 
-const htmlTree = React.createElement("div",
-    {
-        className: "parent"
-    },
-    [
-        React.createElement("div",
-            {
-                className: "child1"
-            },
-            [React.createElement("h1", {}, "This is heading"), React.createElement("p", {}, "This is paragraph")]
-        ),
-        React.createElement("div",
-            {
-                className: "child2"
-            },
-            [React.createElement("h1", {}, "This is heading"), React.createElement("p", {}, "This is paragraph")]
-        ),
-    ]
-)
+const htmlTree = React.createElement("div", {className: "parent"}, [
+        React.createElement("div", {className: "child1"},[
+        React.createElement("h1", {}, "This is heading"), 
+        React.createElement("p", {}, "This is paragraph")
+    ]),
+        React.createElement("div", {className: "child2"},[
+        React.createElement("h1", {}, "This is heading"), 
+        React.createElement("p", {}, "This is paragraph")
+    ]),
+])
 
 // console.log(heading);
 
