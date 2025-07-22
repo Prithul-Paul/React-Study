5d2a9afc2d10b39a9bc73e02315961c3cce3d4ce
Prithul Paul <ptithulpaul@gmail.com>
2025-07-22 20:09:41 +0530

    Create Header, Resturant Card Component

--- App.js
+++ App.js
@@ -1,37 +1,74 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-// const heading = React.createElement("h1", {
-//     id: "heading", 
-//     className: "heading", 
-//     "data-heading": "h1"
-// }, "Hello world from React!!");
 
-// const htmlTree = React.createElement("div", {className: "parent"}, [
-//         React.createElement("div", {className: "child1"},[
-//         React.createElement("h1", {}, "This is heading"), 
-//         React.createElement("p", {}, "This is paragraph")
-//     ]),
-//         React.createElement("div", {className: "child2"},[
-//         React.createElement("h1", {}, "This is heading"), 
-//         React.createElement("p", {}, "This is paragraph")
-//     ]),
-// ])
 
-const Title = () => <h1>Hello world from React!!</h1>;
-
-const Heading = () => {
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const Header = () => {
     return (
-        <div className="heading">
-            {Title()}
-            <h1>React functional component</h1>
+        <div className="header">
+            <div className="logo-section">
+                <img src="https://marketplace.canva.com/EAGZJ2Na9V0/1/0/1600w/canva-red-and-orange-bold-fast-food-restaurant-animated-logo-0ujmrXVcncU.jpg" />
+            </div>
+            <div className="nav-section">
+                <ul className="navlist">
+                    <li className="nav-item">Home</li>
+                    <li className="nav-item">About Us</li>
+                    <li className="nav-item">Cart</li>
+                </ul>
+            </div>
         </div>
     )
 }
 
-// console.log(heading);
-// console.log(headingjsx);
+const ResCard = () => {
+    return (
+    <div className="card-body">
+        <div className="cardImge"><img src="https://www.shutterstock.com/image-photo/fried-salmon-steak-cooked-green-600nw-2489026949.jpg" /></div>
+        <div className="details">
+            <h1 className="resName">KFC</h1>
+            <h2 className="resName">Biriyani, North-South Indian</h2>
+            <p className="rating">4.6 Stars</p>
+            <span className="timing">38 mins.</span>
+        </div>
+    </div>)
+}
+
+const Body = () => {
+    return (
+        <div className="body-container">
+            <div className="search">Search</div>
+            <div className="res-container">
+                <ResCard/>
+                <ResCard/>
+                {/* <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/>
+                <ResCard/> */}
+            </div>
+        </div>
+    )
+}
 
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(<Heading/>);
+const App = () => {
+    return (
+        <div className="app-container">
+            <Header/>
+            <Body/>
+        </div>
+    )
+}
+
+root.render(<App/>);
